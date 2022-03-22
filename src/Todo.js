30f3de59b70ca634c7fd3a67b502453b2605ea2f
Fang-Wen Lee <66965736+fang-wenlee@users.noreply.github.com>
2022-03-21 21:40:29 -0700

    call api with specific id

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
 const Todo = () => {
+  const incompleted = {
+    color: "red"
+  };
   const { id } = useParams();
-  return <h2>A Todo {id}</h2>;
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((res) => {
+        const responseTodo = res.data;
+        setTodo(responseTodo);
+      });
+  }, []);
+
+  console.log(todo);
+  const { id: todoid, title, completed } = todo;
+  return (
+    <>
+      <h2>
+        {`Todo id: ${todoid} 
+     title : ${title} status: 
+     ${completed}
+     `}
+      </h2>
+      {}
+    </>
+  );
 };
 
 export default Todo;
